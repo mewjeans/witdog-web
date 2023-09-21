d49edcf9958e3087d5b29b003d9db8518fc457ec
Kate <120708446+kateiscoding@users.noreply.github.com>
2023-09-21 16:20:32 +0900

    style: added web font

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,8 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
 import { ResetCSS } from './reset';
 
+
 export const GlobalStyle = createGlobalStyle`
   ${ResetCSS}
+
+  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
 
   html {
     font-size: 62.5%;
