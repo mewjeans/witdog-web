5ba568b2cb09af9120685cf1e074c27f4c3998ea
Kate <120708446+kateiscoding@users.noreply.github.com>
2023-09-26 16:08:58 +0900

    style: changed component name and css

--- src/components/sections/CompanySection.tsx
+++ src/components/sections/CompanySection.tsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import styled from 'styled-components';
+import Circle from '../Circle';
 
 interface CompanySectionProps {
     companyRef: React.MutableRefObject<null>;
@@ -8,14 +9,46 @@ interface CompanySectionProps {
   const CompanyIntroSection: React.FC<CompanySectionProps> = ({ companyRef : companyRefProp }) => {
     return (
       <StyledCompanySection ref={ companyRefProp }>
-        <p>안녕</p>
+         <HeaderText>Smart Pet Care<br />Solution</HeaderText>
+          <p>회사 소개 우리회사는 어떻구여 저쩌구요 회사 소개 우리회사는 어떻구여 저쩌구요회사 소개 우리회사는 어떻구여 저쩌구요회사 소개 우리회사는 어떻구여 저쩌구요회사 소개 우리회사는 어떻구여 저쩌구요</p>
+        <Circle />
       </StyledCompanySection>
+      
     );
   };
 
 const StyledCompanySection = styled.div`
-  background-color: #525252;
+  /* background-color: #525252; */
   height: 116rem;
+
+  > p {
+    width: 50rem;
+    top: 34rem;
+    left: 25rem;
+    font-weight: 100;
+  font-size: 2rem;
+  line-height: 1.4;
+    position: relative;
+  }
+`;
+
+const HeaderText = styled.h2`
+  opacity: 1;
+  top: 0rem;
+
+  @media screen and (max-width: 1440px) {
+    font-size: 6rem;
+    padding-bottom: 3vw;
+  }
+
+  color: #222;
+  position: relative;
+  font-weight: 100;
+  font-size: 9rem;
+  line-height: 1.1;
+  padding-bottom: 3rem;
+  top: 34rem;
+  left: 24rem;
 `;
 
 export default CompanyIntroSection;
--- src/components/sections/DemoSection.tsx
+++ src/components/sections/DemoSection.tsx
@@ -2,21 +2,21 @@ import React, { useRef } from 'react';
 import styled from 'styled-components';
 
 interface DemoSectionProps {
-    demoRef: React.MutableRefObject<null>; // 이 부분을 수정해야 함
+    demoRef: React.MutableRefObject<null>;
   }
   
-  const DemoSection: React.FC<DemoSectionProps> = ({ demoRef }) => {
+  const DemoIntroSection: React.FC<DemoSectionProps> = ({ demoRef }) => {
     return (
-      <StyledDemoSection ref={demoRef}>
-        <p>안녕</p>
-      </StyledDemoSection>
+      <DemoIntro ref={demoRef}>
+        <p>데모 페이지 입니다</p>
+      </DemoIntro>
     );
   };
 
-const StyledDemoSection = styled.div`
+const DemoIntro = styled.div`
   background-color: #141820;
   height: 116rem;
-  /* 추가적인 스타일을 여기에 적용할 수 있습니다. */
+
 `;
 
-export default DemoSection;
+export default DemoIntroSection;
