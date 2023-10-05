2d321234bdbd48262555c2c640207989923d5fa1
Kate <120708446+kateiscoding@users.noreply.github.com>
2023-10-05 15:40:27 +0900

    style: modified css of menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { Paths } from '../constants/path';
 import Link from 'next/link';
 import styled from 'styled-components';
@@ -8,84 +8,71 @@ import witdogLogo from '../../public/assets/witdog-logo.png';
 import { Fonts } from '@/styles';
 
 const Header: React.FC = () => {
-
-
   return (
     <HeaderLayout>
-        <HeaderContainer>
-          <div>
-            <Link href='/home'>
-              <Image src={witdogLogoWhite} alt='witdog-logo-white' />
-            </Link>
-          </div>
+      <HeaderContainer>
+        <div>
+          <Link href='/home'>
+            <Image src={witdogLogoWhite} alt='witdog-logo-white' />
+          </Link>
+        </div>
 
-          <div>
-          <Link href='/intro'>
-              서비스 소개
-            </Link>
-            <Link href='/company'>
-              회사 소개
-            </Link>
-            <Link href='/demo'>
-              데모 영상
-            </Link>
-            <Link href='/download'>
-              앱 다운로드
-            </Link>
-          </div>
-        </HeaderContainer>
+        <div>
+          <Link href='/intro'>서비스 소개</Link>
+          <Link href='/company'>회사 소개</Link>
+          <Link href='/demo'>데모 영상</Link>
+          <Link href='/download'>앱 다운로드</Link>
+        </div>
+      </HeaderContainer>
     </HeaderLayout>
   );
 };
 
 export default Header;
 
-
 const HeaderLayout = styled.header`
-background-color: #000000;
-color: white;
-height: 11rem;
-display: flex;
-flex-direction: row;
-position: fixed;
-top: 0;
-z-index: 1;
-  /* width: 100% */
-left: 0;
-right: 0;
-`
+  background-color: #000000;
+  color: white;
+  height: 11rem;
+  display: flex;
+  flex-direction: row;
+  position: fixed;
+  top: 0;
+  z-index: 1;
+  width: 100%;
+  left: 0;
+  right: 0;
+`;
 
 const HeaderContainer = styled.header`
-background-color: #000000;
-color: white;
-gap: 100rem;
-display: flex;
-
-
-> div:first-child {
-  width: 24rem;
-  position: relative;
-   > a {
-      
-    > img {
-    margin: 4rem;
-    width: 16rem;
-    height: auto;
-    position: absolute;
-   }
-}
-  
-}
-
-> div:last-child {
+  background-color: #000000;
+  color: white;
+  gap: 100rem;
   display: flex;
+
+
+  > div:first-child {
+    width: 24rem;
+    position: relative;
+    > a {
+      > img {
+        margin: 4rem;
+        width: 16rem;
+        height: auto;
+        position: absolute;
+      }
+    }
+  }
+
+  > div:last-child {
+    display: flex;
     flex-direction: row;
     align-items: center;
     gap: 8rem;
     justify-content: flex-end;
 
     > a {
-      ${Fonts.bold18};
+      ${Fonts.medium18};
     }
-}
-`
+  }
+`;
