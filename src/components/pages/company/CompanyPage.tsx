6c071be832586a4f0dd61bd12a366872e9537fee
Kate <120708446+kateiscoding@users.noreply.github.com>
2023-10-12 15:22:29 +0900

    feat: added css layout

--- src/components/pages/company/CompanyPage.tsx
+++ src/components/pages/company/CompanyPage.tsx
@@ -1,3 +1,32 @@
+import styled from "styled-components";
+
 export const CompanyPage = () => {
-    return <div>CompanyPage</div>;
+    return (
+        <CompanyLayout>
+            <CompanyContainer>
+            <div>CompanyPage</div>
+            </CompanyContainer>
+        </CompanyLayout>
+   
+    );
   };
+
+const CompanyLayout = styled.div`
+    width: 100%;
+    height: 105rem;
+    display: flex;
+    /* background-color: #000000; */
+    justify-content: center;
+    top: 11rem;
+    position: fixed;
+`
+
+const CompanyContainer = styled.div`
+    width: 120rem;
+    height: 105rem;
+    background-color: #3f3f3f;
+
+    > div {
+        color: #FFFFFF;
+    }
+`
