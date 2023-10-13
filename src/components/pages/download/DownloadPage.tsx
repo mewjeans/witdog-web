98f4bb2fc5a0cf27b99322fa812495ec751a5d8e
Kate <120708446+kateiscoding@users.noreply.github.com>
2023-10-13 16:05:37 +0900

    feat: added download intro component

--- src/components/pages/download/DownloadPage.tsx
+++ src/components/pages/download/DownloadPage.tsx
@@ -1,32 +1,84 @@
-import styled from "styled-components";
+import React, { useRef } from 'react';
+import styled from 'styled-components';
+import Image from 'next/image';
+import { Fonts } from '@/styles';
+import { DownloadImg } from '../../../../public/svgs';
 
-  export const DownloadPage = () => {
-      return (
-          <DownloadLayout>
-              <DownloadContainer>
-              <div>DownloadPage</div>
-              </DownloadContainer>
-          </DownloadLayout>
-     
-      );
-    };
-  
-  const DownloadLayout = styled.div`
-      width: 100%;
-      height: 105rem;
-      display: flex;
-      /* background-color: #000000; */
-      justify-content: center;
-      top: 11rem;
-      position: fixed;
-  `
-  
-  const DownloadContainer = styled.div`
-      width: 120rem;
-      height: 105rem;
-      background-color: #3f3f3f;
-  
-      > div {
-          color: #FFFFFF;
+export const DownloadPage = () => {
+  return (
+    <DownloadLayout>
+     <DownloadContainer>
+        <div>
+          <p>앱 다운로드</p>
+          <p>
+            반려견 언어를 이해하고,
+            <br />
+            정확히 소통하는 실시간 채팅 서비스 윗독
+            <br /> 지금 바로 윗독하세요!
+          </p>
+          <a href='https://play.google.com/store/apps' target='_blank' rel='noopener noreferrer'>
+            <button>
+              <p>다운로드 버튼</p>
+            </button>
+          </a>
+        </div>
+
+        <Image src={DownloadImg} alt='downloadImage' width={600} />
+      </DownloadContainer>
+    </DownloadLayout>
+  );
+};
+
+const DownloadLayout = styled.div`
+  width: 100%;
+  height: 105rem;
+  display: flex;
+  /* background-color: #000000; */
+  justify-content: center;
+  top: 11rem;
+  position: fixed;
+`;
+
+const DownloadContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  /* width: 120rem; */
+  margin-top: 20rem;
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    gap: 6rem;
+
+    > p {
+      color: #ffffff;
+      ${Fonts.bold20};
+    }
+
+    > p:nth-child(2) {
+      color: #ffffff;
+      ${Fonts.bold34};
+      line-height: 5rem;
+    }
+
+    > a {
+      button {
+        background-color: #88fd92;
+        border-radius: 3rem;
+        width: 16rem;
+        height: 5rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        > p {
+          display: flex;
+        }
       }
-  `
+    }
+  }
+
+  > img {
+    display: flex;
+  }
+`;
